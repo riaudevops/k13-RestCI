1f912c6ff7fa5b2c7da8261c4112ff80b78d15a1
Rez <moehamadrezha@gmail.com>
2019-09-26 19:10:20 +0700

    Update Siswa_API_model.php
    Mengubah data yang dikirimkan oleh endpoint, menjadi seluruh data siswa

--- application/models/Siswa_API_model.php
+++ application/models/Siswa_API_model.php
@@ -3,8 +3,9 @@
 class Siswa_API_model extends CI_Model
 {
     public function getSiswa($id){
-        $this->db->SELECT('NAMA');
-        $this->db->SELECT('JENIS_KELAMIN');
+        // $this->db->SELECT('NAMA');
+        // $this->db->SELECT('JENIS_KELAMIN');
+        $this->db->SELECT('*');
         $this->db->SELECT('peg_kelas.nama_kelas');
         $this->db->FROM('peg_siswa');
         $this->db->JOIN('peg_kelas_siswa', 'peg_siswa.id_siswa = peg_kelas_siswa.id_siswa','right');
@@ -12,6 +13,7 @@ class Siswa_API_model extends CI_Model
         $this->db->JOIN('peg_kelas','peg_kelas_siswa.id_kelas = peg_kelas.id_kelas','left');
         $this->db->WHERE('peg_kelas_siswa.id_kelas = peg_kelas.id_kelas');
         $this->db->WHERE('peg_mengajar.user_id ='.$id);
+        $this->db->ORDER_BY('peg_siswa.NAMA','asc');
 
         $data = $this->db->get()->result_array();
 
